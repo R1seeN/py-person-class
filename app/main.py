cfbb0e0bd9d66410dfc8f2df6b1fa341f7f4c78d
Anastasia Holovach <asia.ostafiychuk@gmail.com>
2026-01-07 17:07:37 +0100

    done solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    instances_people = []
+
+    for person in people:
+        instance = Person(person["name"], person["age"])
+
+        if person.get("wife") is not None:
+            instance.wife = person["wife"]
+
+        if person.get("husband") is not None:
+            instance.husband = person["husband"]
+
+        instances_people.append(instance)
+        Person.people[instance.name] = instance
+
+    for instance in instances_people:
+        if hasattr(instance, "wife"):
+            instance.wife = Person.people.get(instance.wife)
+
+        if hasattr(instance, "husband"):
+            instance.husband = Person.people.get(instance.husband)
+
+    return instances_people
