1f864adfa98893fbcc860104b5a8b5e258f48abb
Anastasia Holovach <asia.ostafiychuk@gmail.com>
2026-01-07 19:01:13 +0100

    solved bags

--- app/main.py
+++ app/main.py
@@ -4,22 +4,11 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    instances_people = []
-
-    for person in people:
-        instance = Person(person["name"], person["age"])
-
-        if person.get("wife") is not None:
-            instance.wife = person["wife"]
-
-        if person.get("husband") is not None:
-            instance.husband = person["husband"]
-
-        instances_people.append(instance)
-        Person.people[instance.name] = instance
+    instances_people = [build_person(person) for person in people]
 
     for instance in instances_people:
         if hasattr(instance, "wife"):
@@ -29,3 +18,15 @@ def create_person_list(people: list) -> list:
             instance.husband = Person.people.get(instance.husband)
 
     return instances_people
+
+
+def build_person(person: dict) -> Person:
+    instance = Person(person["name"], person["age"])
+
+    if person.get("wife") is not None:
+        instance.wife = person["wife"]
+
+    if person.get("husband") is not None:
+        instance.husband = person["husband"]
+
+    return instance
